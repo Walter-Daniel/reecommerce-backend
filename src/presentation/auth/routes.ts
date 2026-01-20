efccf38bbf0b6eddccc9e15d06d810f32afb8d83
Walter Carrizo <walterdcarrizo.19@gmail.com>
2026-01-20 08:59:35 -0300

    feat: integrate AuthRepository into AuthController and AuthRoutes for user registration

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import { AuthController } from './controller.js';
+import {
+  AuthDatasourceImpl,
+  AuthRepositoryImpl,
+} from '../../infraestructure/index.js';
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
-    const controller = new AuthController();
+    const datasource = new AuthDatasourceImpl();
+    const authRepository = new AuthRepositoryImpl(datasource);
+
+    const controller = new AuthController(authRepository);
 
     //Definir todas las rutas principales
 
